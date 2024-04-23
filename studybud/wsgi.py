f0fd2bf974fd78250889014d7f814be62e0607a6
HrsFlex <harsh42026@gmail.com>
2024-04-24 02:25:03 +0530

    removing vercel config

--- studybud/wsgi.py
+++ studybud/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for studybud project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -15,4 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
 
 application = get_wsgi_application()
 
-app = application
